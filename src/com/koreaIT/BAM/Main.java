9a5eee656f077578a35d0bc6f47cc81d317b0dd2
devCJH <http-@naver.com>
2024-07-08 19:48:44 +0900

    member join

--- src/com/koreaIT/BAM/Main.java
+++ src/com/koreaIT/BAM/Main.java
@@ -31,7 +31,7 @@ public class Main {
 		
 		while (true) {
 			System.out.printf("명령어) ");
-			String cmd = sc.nextLine();
+			String cmd = sc.nextLine().trim();
 
 			if (cmd.equals("exit")) {
 				break;
@@ -39,26 +39,68 @@ public class Main {
 			
 			if (cmd.equals("member join")) {
 				
-				System.out.printf("아이디 : ");
-				String loginId = sc.nextLine();
-				
+				String loginId = null;
 				String loginPw = null;
+				String name = null;
+				
+				while (true) {
+					System.out.printf("아이디 : ");
+					loginId = sc.nextLine().trim();
+					
+					if (loginId.length() == 0) {
+						System.out.println("아이디는 필수 입력 정보입니다");
+						continue;
+					}
+					
+					boolean isLoginIdDup = false;
+					
+					for (Member member : members) {
+						if (member.getLoginId().equals(loginId)) {
+							isLoginIdDup = true;
+							break;
+						}
+					}
+					
+					if (isLoginIdDup == true) {
+						System.out.printf("[ %s ]은(는) 이미 사용중인 아이디입니다\n", loginId);
+						continue;
+					}
+					
+					System.out.printf("[ %s ]은(는) 사용가능한 아이디입니다\n", loginId);
+					break;
+				}
+				
 				while (true) {
 					System.out.printf("비밀번호 : ");
-					loginPw = sc.nextLine();
+					loginPw = sc.nextLine().trim();
+					
+					if (loginPw.length() == 0) {
+						System.out.println("비밀번호는 필수 입력 정보입니다");
+						continue;
+					}
+					
 					System.out.printf("비밀번호 확인 : ");
-					String loginPwChk = sc.nextLine();
+					String loginPwChk = sc.nextLine().trim();
 					
 					if (loginPw.equals(loginPwChk) == false) {
 						System.out.println("비밀번호가 일치하지 않습니다");
 						continue;
 					}
 					
+					break;
+				}
+				
+				while (true) {
+					System.out.printf("이름 : ");
+					name = sc.nextLine().trim();
+					
+					if (name.length() == 0) {
+						System.out.println("이름은 필수 입력 정보입니다");
+						continue;
+					}
 					
 					break;
 				}
-				System.out.printf("이름 : ");
-				String name = sc.nextLine();
 				
 				Member member = new Member(++lastMemberId, Util.getDateStr(), Util.getDateStr(), loginId, loginPw, name);
 				members.add(member);
